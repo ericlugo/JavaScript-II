a2870483ae1b4854afacf41d4f7f8ef6058cf0fb
Eric Lugo <ericlugo@users.noreply.github.com>
2019-04-01 00:12:40 -0400

    MVP Complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,21 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function makeExponent(y, x){
+  return function(x) {
+    return Math.pow(x, y);
+  }
+}
+const Pow1 = makeExponent(1);   console.log(Pow1(2));
+const Pow2 = makeExponent(2);   console.log(Pow2(2));
+const Pow3 = makeExponent(3);   console.log(Pow3(2));
+const Pow4 = makeExponent(4);   console.log(Pow4(2));
+const Pow5 = makeExponent(5);   console.log(Pow5(2));
+const Pow6 = makeExponent(6);   console.log(Pow6(2));
+const Pow7 = makeExponent(7);   console.log(Pow7(2));
+const Pow8 = makeExponent(8);   console.log(Pow8(2));
+const Pow9 = makeExponent(9);   console.log(Pow9(2));
+const Pow10 = makeExponent(10); console.log(Pow10(2));
+
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
